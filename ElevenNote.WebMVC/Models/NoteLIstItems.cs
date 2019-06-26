2507661bef0f5d8c870ef164dfc1f4e13ec13d54
aknight9255 <amanda.knight55@gmail.com>
2019-06-26 11:21:47 -0400

    Added Index view Notes

---
+++ ElevenNote.WebMVC/Models/NoteLIstItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.WebMVC.Models
+{
+    public class NoteListItems
+    {
+        public int NoteId { get; set; }
+        public string Title { get; set; }
+        [Display(Name ="Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
